2cf3fb7ebb61e9c20a90ca61170eef4aa49684be
Solotov <p.matheus.santos@uni9.edu.br>
2025-10-06 22:26:40 +0000

    testando algumas coisas

--- index.php
+++ index.php
@@ -3,13 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Programação Back-End - PHP</title>
+    <title>Testes em PHP</title>
 </head>
 <body>
     <h1>Testando php</h1>
-    <?php
-    
-    echo " <br>  <a type='button' href='second_classrom.php'>aula2</a>";
-    ?>
+   <section>
+       <div>
+            <form action="#" method="get">
+                <ul>
+                    <li>
+                        <label for="nome">Nome</label>
+                        <input type="text" id="nome" name="nome" placeholder="digite seu nome">
+                    </li>
+                </ul>
+            </form>
+       </div> 
+   </section>
 </body>
 </html>
