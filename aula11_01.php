534a6a4777f92015bde63f408346ac824f718b37
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2025-10-30 14:13:34 +0000

    Aula11

--- aula11_01.php
+++ aula11_01.php
@@ -28,12 +28,38 @@
     echo"<br>y[2]=".$y[2];
     echo "<hr>";
     $estudante=[
+        "id"=>1,
         "ra"=>1234,
         "nome"=>"Bete",
         "av1"=>8,
         "av2"=>7.5
     ];
-    echo "<br>Estudante: ".$estudante[1];
+    /* Mostrar todos os dados do(a) estudante e 
+        calcular a media entre av1 e av2*/
+        echo "<br>Id: ".$estudante["id"];
+        echo "<br>Nome: ".$estudante["nome"];
+        echo "<br>Ra: ".$estudante["ra"];
+        echo "<br>Av1: ".$estudante["av1"];
+        echo "<br>Av2: ".$estudante["av2"];
+        $media=($estudante["av1"]+$estudante["av2"])/2;
+        echo "<br>Media: $media ";
+
+        $num=[
+              [1,2,3],
+              [4,5,6],
+              [7,8,9]  
+        ];
+        echo "<br>num =".$num[1][0];
+        $naval=[
+            ["água","água","submarino","água"],
+            ["veleiro","água","água","água"],
+            ["água","água","água","avião"],
+            ["água","trem","água","água"]
+        ];
+        // imprimir: veleiro trem submarino avião
+        echo "<hr>";
+        echo $naval[1][0]." ".$naval[3][1]." ".$naval[0][2]." ".$naval[2][3];
+
     ?>
 </body>
 </html>
